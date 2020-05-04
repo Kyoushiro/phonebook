d1c9c2d56a668a491167bd15025673b04def281d
oskari <oskari.ilkka@hotmail.com>
2020-05-04 15:10:30 +0300

    Updates

---
+++ precache-manifest.cace4849096af10f2c06aea6bd007239.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f590f2a2257cfc9348efb5e2470fc98",
+    "url": "/phonebook/index.html"
+  },
+  {
+    "revision": "bf63465f9375162e3bc9",
+    "url": "/phonebook/static/css/main.82960e02.chunk.css"
+  },
+  {
+    "revision": "c8a21a23adce0475e3b3",
+    "url": "/phonebook/static/js/2.f183514c.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/phonebook/static/js/2.f183514c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf63465f9375162e3bc9",
+    "url": "/phonebook/static/js/main.c64c7163.chunk.js"
+  },
+  {
+    "revision": "0f28676ee9d514f7de95",
+    "url": "/phonebook/static/js/runtime-main.b60f526a.js"
+  },
+  {
+    "revision": "ce8e2a4156448bf45ac70338ba0093f0",
+    "url": "/phonebook/static/media/accept.ce8e2a41.svg"
+  },
+  {
+    "revision": "713fe29d10415a51910d79f66c21d161",
+    "url": "/phonebook/static/media/arrowLeft.713fe29d.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/phonebook/static/media/cancel.2c5552a1.svg"
+  },
+  {
+    "revision": "da29699872e6e39ddc9eec57abca33fd",
+    "url": "/phonebook/static/media/delete.da296998.svg"
+  },
+  {
+    "revision": "7465375f8d26822a4aefcd9c0bba538f",
+    "url": "/phonebook/static/media/user-plus-solid.7465375f.svg"
+  }
+]);
